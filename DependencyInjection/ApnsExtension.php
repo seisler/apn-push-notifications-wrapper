dc0e0662612bea23ff55532e3ebd0e438e52e094
seisler <markseisler@gmail.com>
2016-11-14 14:53:34 +0100

    Renaming extension name to allow the auto detection

--- DependencyInjection/ApnsExtension.php
+++ DependencyInjection/ApnsExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class ApnsPHPSymfonyWrapperExtension extends Extension
+class ApnsExtension extends Extension
 {
 
     /**
